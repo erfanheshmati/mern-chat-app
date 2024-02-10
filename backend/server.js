859e04ab5228185759fa2d43d5ff63d4eca801e1
Erfan Heshmati <heshmati74@gmail.com>
2024-02-10 16:41:04 +0330

    project initial setup

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
