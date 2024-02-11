77383b5f256d4cf9f42ec395e0e0491f2dc6460e
Erfan Heshmati <heshmati74@gmail.com>
2024-02-11 11:29:02 +0330

    create message and conversation model

---
+++ backend/models/messageModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const messageSchema = mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    recieverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    message: {
+      type: String,
+      require: true,
+    },
+  },
+  // createdAt, updatedAt
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
