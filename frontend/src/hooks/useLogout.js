4d2d997fe8a1ad1f2f49ad7c1adb01dc7d6c1504
Erfan Heshmati <heshmati74@gmail.com>
2024-02-13 10:22:48 +0330

    send message functionality

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
+import toast from "react-hot-toast";
 
 export default function useLogout() {
   const [loading, setLoading] = useState(false);
